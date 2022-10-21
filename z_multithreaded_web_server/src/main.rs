53fbf88ae9613bcf5f8e8fe93aa9585f2d97fd81
Honoluluchampi <msd13aj@gmail.com>
2022-10-22 02:24:26 +0900

    asynchronous server

--- z_multithreaded_web_server/src/main.rs
+++ z_multithreaded_web_server/src/main.rs
@@ -1,3 +1,5 @@
+use z_multithreaded_web_server::ThreadPool;
+
 use std::{
     fs,
     io::{ prelude::*, BufReader },
@@ -8,13 +10,14 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        println!("connecting...");
         let stream = stream.unwrap();
-        println!("connection established!");
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
